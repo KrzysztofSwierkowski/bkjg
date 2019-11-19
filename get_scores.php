602a9eb4de8c25a84046f86931c1fef77944faeb
KrzysztofSwierkowski <Swierkowski.K@gmail.com>
2019-11-19 21:54:33 +0100

    zmiany
    dostosowaniue

--- get_scores.php
+++ get_scores.php
@@ -1,44 +1,45 @@
 <?php
-require_once 'login.php';
+  require_once 'login.php';
 
-  $conn = new mysqli($hn, $un, $pw, $db);
-  if ($conn->connect_error) die("Błąd krytyczny");
+    $conn = new mysqli($hn, $un, $pw, $db);
+    if ($conn->connect_error) die("Błąd krytyczny");
 
-  $tableName = $_POST['tableName'];
+    $tableName = 'zawody';//$_POST['tableName'];
 
-$sql = "SELECT nazwisko, imie, klub, seria1, seria2, seria3, seria4, seria5, seria6, dziesiatkiW, dziesiatki FROM $tableName";
-  $result = $conn->query($sql);
+   $sql = "SELECT nazwisko, imie, klub, seria1, seria2, seria3, seria4, seria5, seria6, dziesiatkiW, dziesiatki FROM $tableName";
+    $result = $conn->query($sql);
 
-  if ($result->num_rows > 0) {
-    // output data of each row
+    if ($result->num_rows > 0) {
+      // output data of each row
 
- echo   '<form method="post" action="store_detail.php">
-    <table id="score_table" align=center>
-            <tr id="row1">
-                  <td><input type="text" name="tableName" placeholder="Wpisz nową nazwę zawodów"></td>';
+   echo   '<form method="post" action="store_detail.php">
+      <table id="score_table" align=center>
+              <tr id="row1">
+                    <td><input type="text" name="tableName" placeholder="Wpisz nową nazwę zawodów"></td>';
 
-    while($row = $result->fetch_assoc()) {
-        echo "Nazwisko: " . $row["nazwisko"]. " - Imię: " . $row["imie"]. " " . $row["klub"]. "<br>";
-        echo '                </tr>
-         <tr id="row1">
-          <td><input type="text" name="nazwisko[]" placeholder="Wpisz Nazwisko" value = ' . $row['nazwisko']. '></td>
-          <td><input type="text" name="imie[]" placeholder="Wpisz Imię" value = ' . $row['imie']. '></td>
-          <td><input type="text" name="klub[]" placeholder="Wpisz Klub" value = ' . $row['klub']. '></td>
-          <td><input type="text" name="seria1[]" placeholder="Seria 1" value = ' . $row['seria1']. '></td>
-          <td><input type="text" name="seria2[]" placeholder="Seria 2" value = ' . $row['seria2']. '></td>
-          <td><input type="text" name="seria3[]" placeholder="Seria 3" value = ' . $row['seria3']. '></td>
-          <td><input type="text" name="seria4[]" placeholder="Seria 4" value = ' . $row['seria4']. '></td>
-          <td><input type="text" name="seria5[]" placeholder="Seria 5" value = ' . $row['seria5']. '></td>
-          <td><input type="text" name="seria6[]" placeholder="Seria 6" value = ' . $row['seria6']. '></td>
-          <td><input type="text" name="dziesiatkiW[]" placeholder="Dziesiątki wewn." value = ' . $row['dziesiatkiW']. '></td>
-          <td><input type="text" name="dziesiatki[]" placeholder="Dziesiątki" value = ' . $row['dziesiatki']. '></td>
-          
-         </tr>';
+           while($row = $result->fetch_assoc()) {
+        
+          echo '                </tr>
+          <tr id="row1">
+            <td><input type="text" name="nazwisko[]" placeholder="Wpisz Nazwisko" value = ' . $row['nazwisko']. '></td>
+            <td><input type="text" name="imie[]" placeholder="Wpisz Imię" value = ' . $row['imie']. '></td>
+            <td><input type="text" name="klub[]" placeholder="Wpisz Klub" value = ' . $row['klub']. '></td>
+            <td><input type="text" name="seria1[]" placeholder="Seria 1" value = ' . $row['seria1']. '></td>
+            <td><input type="text" name="seria2[]" placeholder="Seria 2" value = ' . $row['seria2']. '></td>
+            <td><input type="text" name="seria3[]" placeholder="Seria 3" value = ' . $row['seria3']. '></td>
+            <td><input type="text" name="seria4[]" placeholder="Seria 4" value = ' . $row['seria4']. '></td>
+            <td><input type="text" name="seria5[]" placeholder="Seria 5" value = ' . $row['seria5']. '></td>
+            <td><input type="text" name="seria6[]" placeholder="Seria 6" value = ' . $row['seria6']. '></td>
+            <td><input type="text" name="dziesiatkiW[]" placeholder="Dziesiątki wewn." value = ' . $row['dziesiatkiW']. '></td>
+            <td><input type="text" name="dziesiatki[]" placeholder="Dziesiątki" value = ' . $row['dziesiatki']. '></td>
+            
+          </tr>';
 
+      }
     }
-    echo    '</table>
-    <input type="button" onclick="add_row();" value="Dodaj pozycje">
-    <input type="submit" name="submit_row" value="ZAPISZ">
-   </form>';
+      echo    '</table>
+      <input type="button" onclick="add_row();" value="Dodaj pozycje">
+      <input type="submit" name="submit_row" value="ZAPISZ">
+       </form>';
 
   ?>
--- store_detail.php
+++ store_detail.php
@@ -23,42 +23,44 @@ if(isset($_POST['submit_row']))
      seria6 varchar(255),
      dziesiatkiW varchar(255),
      dziesiatki varchar(255));
-   ";	
+        ";	
  
-   $result = $conn->query($sql);
-   if (!$result) echo "Instrukcja nie powiodła się.<br><br>";
-    elseif ($result) echo "Instrukcja powiodła się.<br><br>";
-    echo $result;
-    echo '<br> -------- <br>';
+       $result = $conn->query($sql);
+       if (!$result) echo "Instrukcja nie powiodła się.<br><br>";
+        elseif ($result) echo "Instrukcja powiodła się.<br><br>";
+        echo $result;
+        echo '<br> -------- <br>';
 
- $nazwisko=$_POST['nazwisko'];
- $imie=$_POST['imie'];
- $klub=$_POST['klub'];
- $seria1=$_POST['seria1'];
- $seria2=$_POST['seria2'];
- $seria3=$_POST['seria3'];
- $seria4=$_POST['seria4'];
- $seria5=$_POST['seria5'];
- $seria6=$_POST['seria6'];
- $dziesiatkiW=$_POST['dziesiatkiW'];
- $dziesiatki=$_POST['dziesiatki'];
+          $nazwisko=$_POST['nazwisko'];
+          $imie=$_POST['imie'];
+          $klub=$_POST['klub'];
+          $seria1=$_POST['seria1'];
+          $seria2=$_POST['seria2'];
+          $seria3=$_POST['seria3'];
+          $seria4=$_POST['seria4'];
+          $seria5=$_POST['seria5'];
+          $seria6=$_POST['seria6'];
+          $dziesiatkiW=$_POST['dziesiatkiW'];
+          $dziesiatki=$_POST['dziesiatki'];
 
  
- for($i=0;$i<count($nazwisko);$i++)
- {
-  if($nazwisko[$i]!="" && $imie[$i]!="" && $seria1[$i]!="")
-  {
-    $query = "insert into $tableName values('$nazwisko[$i]','$imie[$i]','$klub[$i]','$seria1[$i]','$seria2[$i]','$seria3[$i]','$seria4[$i]','$seria5[$i]','$seria6[$i]','$dziesiatkiW[$i]','$dziesiatki[$i]')";	 
-    $result = $conn->query($query);
-    echo '<br>R2: -------- <br>';
-    echo $result;
-    echo '<br>q: -------- <br>';
-    echo $query;
-    if (!$result) echo "Instrukcja nie powiodła się.<br><br>";
-   elseif ($result) echo "Instrukcja powiodła się.<br><br>";
- }
-}
+          for($i=0;$i<count($nazwisko);$i++)
+          {
+          if($nazwisko[$i]!="" && $imie[$i]!="" && $seria1[$i]!="")
+          {
+          $query = "insert into $tableName values('$nazwisko[$i]','$imie[$i]','$klub[$i]','$seria1[$i]','$seria2[$i]','$seria3[$i]','$seria4[$i]','$seria5[$i]','$seria6[$i]','$dziesiatkiW[$i]','$dziesiatki[$i]')";	 
+          $result = $conn->query($query);
+          echo '<br>R2: -------- <br>';
+          echo $result;
+          echo '<br>q: -------- <br>';
+          echo $query;
+          if (!$result) echo "Instrukcja nie powiodła się.<br><br>";
+           elseif ($result) echo "Instrukcja powiodła się.<br><br>";
+          }
 
+        }
+      
+    
 }
-}
+ 
 ?>
